e4fccce4bed270949ab545a9b7bee46690ffea8f
mertakanay <mert.akanay@live.com>
2015-04-09 13:41:32 -0700

    search tab completed

---
+++ Instagram/SearchTableCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableCell.h
+//  Instagram
+//
+//  Created by Mert Akanay on 4/8/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableCell : UITableViewCell
+
+@end
---
+++ Instagram/SearchTableCell.m
@@ -0,0 +1,23 @@
+//
+//  SearchTableCell.m
+//  Instagram
+//
+//  Created by Mert Akanay on 4/8/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import "SearchTableCell.h"
+
+@implementation SearchTableCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
