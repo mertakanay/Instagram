c27907fe4a8c1cf00fdc60317ff259a10a9e7580
mertakanay <mert.akanay@live.com>
2015-04-08 18:24:30 -0700

    profile page and main page working

--- Instagram/ProfileViewController.m
+++ Instagram/ProfileViewController.m
@@ -8,7 +8,7 @@
 
 #import "ProfileViewController.h"
 
-@interface ProfileViewController ()
+@interface ProfileViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
 @property (weak, nonatomic) IBOutlet UILabel *followingsLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
 @property (weak, nonatomic) IBOutlet UILabel *fullNameLabel;
@@ -20,16 +20,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.currentUser = [PFUser currentUser];
+    self.currentUser = [User currentUser];
 
     self.followingsLabel.text = [NSString stringWithFormat:@"%lu following",(unsigned long)self.currentUser.followingArray.count];
-    self.fullNameLabel.text = self.currentUser.fullName;
-
+    self.fullNameLabel.text = self.currentUser.username;
+    NSLog(@"%@",self.currentUser.username);
         [self.currentUser.profileImage getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
             if (!error) {
                 UIImage *image = [UIImage imageWithData:data];
                 self.profileImageView.image = image;
             }
         }];
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 0;
 }
 @end
